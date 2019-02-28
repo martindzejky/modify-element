332d570964f93c740ff1a92e254f04bc7c085903
Martin Jakubik <martin@jakubik.info>
2019-02-28 14:01:41 +0100

    add rollup bundler

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+const { ENV } = process.env;
+
+module.exports = {
+    input: './src/index.ts',
+    output: {
+        name: 'ModifyElement',
+        file: './dist/modify-element.js',
+        format: 'iife',
+        sourcemap: ENV === 'production',
+    },
+    watch: {
+        clearScreen: false,
+    },
+    plugins: [
+        require('rollup-plugin-node-resolve')({
+            browser: true,
+        }),
+        require('rollup-plugin-commonjs')({
+            include: /node_modules/,
+        }),
+
+        require('rollup-plugin-typescript2')({
+            rollupCommonJSResolveHack: true,
+        }),
+
+        ...(ENV === 'production'
+            ? [require('rollup-plugin-uglify').uglify()]
+            : []),
+    ],
+};
